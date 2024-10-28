b023aef5d9e80027f56d4e381e12f1ac0f00c4d8
Monique de Jesus Silva <rizzarddirizu@gmail.com>
2024-10-28 10:17:56 -0400

    feat: adicionando componente hook para detectar media-query.

---
+++ src/hooks/useScreenDetector.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export const useScreenDetector = () => {
+  const [width, setWidth] = useState<number | null>(null);
+
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    handleWindowSizeChange();
+
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const isMobile = width !== null && width <= 768;
+  const isTablet = width !== null && width <= 1024;
+  const isDesktop = width !== null && width > 1024;
+
+  return { isMobile, isTablet, isDesktop };
+};
