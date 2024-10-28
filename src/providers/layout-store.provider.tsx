14e9afc187e5350fe77946f699ecbc2f7eae3087
Monique de Jesus Silva <rizzarddirizu@gmail.com>
2024-10-28 10:20:01 -0400

    feat: adicionando componente para prover o componente layout.store.

---
+++ src/providers/layout-store.provider.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { type ReactNode, createContext, useRef, useContext } from "react";
+import { useStore } from "zustand";
+
+import {
+  type LayoutStore,
+  createLayoutStore,
+  initLayoutStore,
+} from "@/stores/layout.store.ts";
+
+export type LayoutStoreApi = ReturnType<typeof createLayoutStore>;
+
+export const LayoutStoreContext = createContext<LayoutStoreApi | undefined>(
+  undefined
+);
+
+export interface LayoutStoreProviderProps {
+  children: ReactNode;
+}
+
+export const LayoutStoreProvider = ({ children }: LayoutStoreProviderProps) => {
+  const storeRef = useRef<LayoutStoreApi>();
+  if (!storeRef.current) {
+    storeRef.current = createLayoutStore(initLayoutStore());
+  }
+
+  return (
+    <LayoutStoreContext.Provider value={storeRef.current}>
+      {children}
+    </LayoutStoreContext.Provider>
+  );
+};
+
+export function useLayoutStore<T = LayoutStore>(
+  selector?: (store: LayoutStore) => T
+): T {
+  const layoutStoreContext = useContext(LayoutStoreContext);
+
+  if (!layoutStoreContext) {
+    throw new Error(`useLayoutStore must be used within LayoutStoreProvider`);
+  }
+  const defaultSelector: (store: LayoutStore) => T = (x) => x as any;
+
+  return useStore(layoutStoreContext, selector ?? defaultSelector);
+}
