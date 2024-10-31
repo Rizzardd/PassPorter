841666de9f70c7d60a395ee07123e1066bc02112
Monique de Jesus Silva <rizzarddirizu@gmail.com>
2024-10-31 02:10:46 -0400

    refactor: adicionando o Auth e fazendo novas estilizações no componente.

--- src/components/app/app-headbar.tsx
+++ src/components/app/app-headbar.tsx
@@ -6,61 +6,78 @@ import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from "../ui/menu";
 import { TbSquareRoundedChevronDownFilled } from "react-icons/tb";
 import { HiOutlinePlus } from "react-icons/hi";
 import { MenuToggleButton } from "./menu-toggle-button";
+import { useRouter } from "next/router";
 
 export function AppHeadbar({}: React.PropsWithChildren<{}>) {
-  return (
-    <Flex
-      direction="row"
-      bg="rgba(0,0,0,0.8)"
-      h="80px"
-      alignItems="center"
-      justify="center"
-      backdropFilter="saturate(180%) blur(9px)"
+  const navigate = useRouter();
+  const isAuth = navigate.route.includes("/auth");
 
-    >
-      <MenuToggleButton/>
-      <Flex h="52px" maxW="1100px" w="100%">
+  return (
+    <div>
+      {!isAuth ? (
         <Flex
-          justify="flex-start"
+          direction="row"
+          bg="rgba(0,0,0,0.8)"
+          h="80px"
           alignItems="center"
-          flexBasis="50%"
-          flexShrink={0}
+          justify="center"
+          backdropFilter="saturate(180%) blur(9px)"
         >
-          <Image
-            mr="16px"
-            src="https://fakeimg.pl/100x46/ffffff/454545?text=Logo&font=noto"
-            h="40px"
-            className="hidden"
-          />
-          <Button mr="16px" hidden>Home</Button>
-          <Button mr="16px" hidden>Eventos</Button>
-        </Flex>
-        <Flex
-          justify="flex-end"
-          flexShrink={0}
-          flexBasis="50%"
-          alignItems="center"
-        >
-          <Button bg="white" color="black" px="16px" mr="16px" className="hidden">
-            Criar Eventos
-          </Button>
-          <MenuRoot >
-            <MenuTrigger asChild className="mr-[1rem]">
-              <Button>
-                <Avatar src="https://avatar.iran.liara.run/public" />
-                <TbSquareRoundedChevronDownFilled />
+          <MenuToggleButton />
+          <Flex h="52px" maxW="1100px" w="100%">
+            <Flex
+              justify="flex-start"
+              alignItems="center"
+              flexBasis="50%"
+              flexShrink={0}
+            >
+              <Image
+                mr="16px"
+                src="https://fakeimg.pl/100x46/ffffff/454545?text=Logo&font=noto"
+                h="40px"
+                className="hidden"
+              />
+              <Button mr="16px" hidden>
+                Home
               </Button>
-            </MenuTrigger>
-            <MenuContent>
-              <MenuItem value="new-txt">New Text File</MenuItem>
-              <MenuItem value="new-file">New File...</MenuItem>
-              <MenuItem value="new-win">New Window</MenuItem>
-              <MenuItem value="open-file">Open File...</MenuItem>
-              <MenuItem value="export">Export</MenuItem>
-            </MenuContent>
-          </MenuRoot>
+              <Button mr="16px" hidden>
+                Eventos
+              </Button>
+            </Flex>
+            <Flex
+              justify="flex-end"
+              flexShrink={0}
+              flexBasis="50%"
+              alignItems="center"
+            >
+              <Button
+                bg="white"
+                color="black"
+                px="16px"
+                mr="16px"
+                className="hidden"
+              >
+                Criar Eventos
+              </Button>
+              <MenuRoot>
+                <MenuTrigger asChild className="mr-[1rem]">
+                  <Button>
+                    <Avatar src="https://avatar.iran.liara.run/public" />
+                    <TbSquareRoundedChevronDownFilled />
+                  </Button>
+                </MenuTrigger>
+                <MenuContent>
+                  <MenuItem value="new-txt">New Text File</MenuItem>
+                  <MenuItem value="new-file">New File...</MenuItem>
+                  <MenuItem value="new-win">New Window</MenuItem>
+                  <MenuItem value="open-file">Open File...</MenuItem>
+                  <MenuItem value="export">Export</MenuItem>
+                </MenuContent>
+              </MenuRoot>
+            </Flex>
+          </Flex>
         </Flex>
-      </Flex>
-    </Flex>
+      ) : null}
+    </div>
   );
 }
