adb2fa7cb8e6df4195e3063f4f92bcdc4fb55df1
Wendy Hemlock <wendyhemlock@gmail.com>
2024-11-06 18:19:24 -0400

    feat: Criação inicial de feature de Autenticação (Ainda não implementada)

---
+++ src/core/users/types/index.ts
@@ -0,0 +1,15 @@
+export interface UserLogin {
+  id: string
+  username: string
+  password: string
+  email: string
+}
+
+export interface User {
+  id: string
+  name: string
+  surname: string
+  email: string
+  username: string
+  age: number
+}
