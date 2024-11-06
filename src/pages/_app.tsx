f40e5a471c117b2620ee006b86509ed12d4685c7
Wendy Hemlock <wendyhemlock@gmail.com>
2024-11-06 18:53:04 -0400

    feat: Adição inicial do form

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,36 @@
-import { AppHeadbar } from "@/components/app/app-headbar";
-import "@/styles/globals.css";
-import { Provider } from "@/components/ui/provider";
-import type { AppProps } from "next/app";
-import { Flex, Text } from "@chakra-ui/react";
-import { LayoutStoreProvider } from "@/providers/layout-store.provider";
-import AppSidebar from "@/components/app/app-sidebar";
+import { AppHeadbar } from '@/components/app/app-headbar'
+import '@/styles/globals.css'
+import { Provider } from '@/components/ui/provider'
+import type { AppProps } from 'next/app'
+import { Flex, Text } from '@chakra-ui/react'
+import { LayoutStoreProvider } from '@/providers/layout-store.provider'
+import AppSidebar from '@/components/app/app-sidebar'
+import useAuthStore from '@/core/users/stores/useAuthStore'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
+const protectedRoutes = ['/booking/booking-details']
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const { isLoggedIn, isLoading, checkAuth } = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [])
+
+  useEffect(() => {
+    if (
+      !isLoading &&
+      !isLoggedIn &&
+      protectedRoutes.includes(router.pathname)
+    ) {
+      router.push('/auth/login')
+    }
+  }, [isLoggedIn, isLoading, router.pathname])
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
   return (
     <Provider>
       <LayoutStoreProvider>
@@ -14,7 +38,6 @@ export default function App({ Component, pageProps }: AppProps) {
           <AppHeadbar />
           <AppSidebar>
             <div>
-              {" "}
               <Flex
                 h="20"
                 alignItems="center"
@@ -36,5 +59,5 @@ export default function App({ Component, pageProps }: AppProps) {
         </Flex>
       </LayoutStoreProvider>
     </Provider>
-  );
+  )
 }
--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,10 +1,67 @@
-import { InputGroup } from "@/components/ui/input-group";
-import { Button, Input } from "@chakra-ui/react";
-import Image from "next/image";
-import { IoIosPin } from "react-icons/io";
-import { IoLogInOutline, IoMailOutline, IoTicket } from "react-icons/io5";
+import { InputGroup } from '@/components/ui/input-group'
+import { Button, Input } from '@chakra-ui/react'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import Image from 'next/image'
+import { IoIosPin } from 'react-icons/io'
+import { IoLogInOutline, IoMailOutline, IoTicket } from 'react-icons/io5'
+import { useForm } from 'react-hook-form'
+import { useState } from 'react'
+
+interface FormData {
+  input: string
+  password: string
+}
+
+const schema = yup.object().shape({
+  input: yup.string().required('Email ou Usuário são obrigatórios'),
+  password: yup
+    .string()
+    .min(8, 'Senha precisa ter no minimo 8 caracteres')
+    .required('Senha é obrigatório'),
+})
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  })
+
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
+
+  const onSubmit = async (data: FormData) => {
+    setErrorMessage(null)
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/authenticateUser`,
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            login: data.input,
+            password: data.password,
+          }),
+        }
+      )
+
+      if (!response.ok) {
+        const errorData = await response.json()
+        setErrorMessage(errorData.message || 'Login failed')
+        return
+      }
+
+      const result = await response.json()
+      console.log(result.message)
+    } catch (error) {
+      console.error('Login error:', error)
+      setErrorMessage('An error occurred during login.')
+    }
+  }
   return (
     <main className="bg-dark-grey w-full">
       <div className="font-display text-white font-extrabold text-[5.5vw] mx-auto w-fit text-center">
@@ -28,13 +85,12 @@ export default function Login() {
       </h1>
 
       <div className="flex flex-col items-center gap-4 my-[2rem]">
-        
-      <div className="flex gap-2.5 self-start ml-[10%] ">
-        <p>*</p>
-        <p className="font-display text-white font-thin text-[4.5vw] w-fit">
-          Email ou Usuário
-        </p>
-      </div>
+        <div className="flex gap-2.5 self-start ml-[10%] ">
+          <p>*</p>
+          <p className="font-display text-white font-thin text-[4.5vw] w-fit">
+            Email ou Usuário
+          </p>
+        </div>
         <InputGroup
           className="mx-auto  h-fit  "
           startElement={
@@ -42,21 +98,21 @@ export default function Login() {
               <IoMailOutline className="scale-150" />
             </div>
           }
-          startElementProps={{ color: "white" }}
+          startElementProps={{ color: 'white' }}
         >
           <Input
             placeholder="Digite seu E-mail ou Usuário"
+            {...register('input')}
             className="bg-dark-grey h-[6vh] w-[85vw] font-display rounded-full border-2 border-grey  "
-            
           ></Input>
         </InputGroup>
 
         <div className="flex gap-2.5 self-start ml-[10%] ">
-        <p>*</p>
-        <p className="font-display text-white font-thin text-[4.5vw] w-fit">
-          Senha
-        </p>
-      </div>
+          <p>*</p>
+          <p className="font-display text-white font-thin text-[4.5vw] w-fit">
+            Senha
+          </p>
+        </div>
 
         <InputGroup
           className="mx-auto  h-fit  "
@@ -65,27 +121,31 @@ export default function Login() {
               <IoIosPin className="scale-150" />
             </div>
           }
-          startElementProps={{ color: "white" }}
+          startElementProps={{ color: 'white' }}
         >
           <Input
+            type="password"
+            {...register('password')}
             placeholder="Digite sua Senha"
             className="bg-dark-grey h-[6vh] w-[85vw] font-display rounded-full border-2 border-grey "
-          
           ></Input>
         </InputGroup>
-        <Button className="bg-grey h-[6vh] w-[85vw] font-display rounded-full border-2 border-grey justify-start mt-5 my-2 text-white ">
-           <div className="h-[6vh] w-[6vh] bg-light-green rounded-full flex justify-center items-center   border-2 border-light-green">
-              <IoLogInOutline className="scale-150" />
-            </div>
-            Entrar
+        <Button
+          onClick={handleSubmit(onSubmit)}
+          className="bg-grey h-[6vh] w-[85vw] font-display rounded-full border-2 border-grey justify-start mt-5 my-2 text-white "
+        >
+          <div className="h-[6vh] w-[6vh] bg-light-green rounded-full flex justify-center items-center   border-2 border-light-green">
+            <IoLogInOutline className="scale-150" />
+          </div>
+          Entrar
         </Button>
         <Button className="bg-white h-[6vh] w-[85vw] font-display rounded-full border-2 border-grey justify-start text-grey ">
-           <div className="h-[6vh] w-[6vh] bg-grey rounded-full flex justify-center items-center border-2 border-grey">
-              <IoLogInOutline className="scale-150 text-light-green" />
-            </div>
-            Cadastrar-se
+          <div className="h-[6vh] w-[6vh] bg-grey rounded-full flex justify-center items-center border-2 border-grey">
+            <IoLogInOutline className="scale-150 text-light-green" />
+          </div>
+          Cadastrar-se
         </Button>
       </div>
     </main>
-  );
+  )
 }
