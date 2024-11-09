aa3d8cfecc01a5ec987a7a4aed387eb2d4dd482c
Monique de Jesus Silva <rizzarddirizu@gmail.com>
2024-11-09 18:29:42 -0400

    refactor: ajustes no componente. Importação do componente do card translùcido e estilização.

--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -3,49 +3,49 @@ import Image from 'next/image'
 import { Flex } from '@chakra-ui/react'
 import { Avatar } from '@/components/ui/avatar'
 import { RiVerifiedBadgeFill } from 'react-icons/ri'
+import { IoIosPin } from 'react-icons/io'
+import TranslucentCard from '@/components/app/translucentcard'
 
 export default function EventDetails() {
   return (
     <div>
-      <div className=" justify-items-center h-fit rounded-t-lg relative ">
-        <Image alt="" src={Wallpaper} />
-        <div className="absolute bottom-0 w-full  ">
-          <Flex
-            direction="row"
-            bg="rgba(0,0,0,0.8)"
-            h="125px"
-            alignItems="center"
-            justify="center"
-            backdropFilter="saturate(180%) blur(9px)"
-            className="rounded-t-3xl"
-          >
-            <div className=" flex flex-wrap gap-2  px-5">
-              <Avatar size="2xl" src="https://avatar.iran.liara.run/public" />
-            </div>
-            <div className=" px-2 flex flex-col w-full mx-auto">
-              <div className="flex flex-row w-full gap-2">
-                <h1 className="font-display text-white font-normal text-[15px]">
-                  Tâmara Salles
-                </h1>
-                <RiVerifiedBadgeFill className="mt-[3px] text-light-green" />
-              </div>
-              <p className="font-display text-white font-thin text-[13px]">
-                Organizador
-              </p>
-            </div>
-          </Flex>
+      <TranslucentCard />
+      <div className=" bg-dark-grey w-full h-full rounded-t-3xl px-2 py-4  flex ">
+        <div className="flex flex-col">
+          <div className="rounded-t-lg bg-light-purple w-[60px] h-[30px] flex text-center items-center">
+            <p className="font-display text-white font-normal text-[15px] p-4">
+              Mar
+            </p>
+          </div>
+          <div className="bg-white rounded-b-lg  w-[60px] h-[30px] flex text-center items-center">
+            <p className="font-display text-black font-bold text-[20px] p-[1.20rem]">
+              29
+            </p>
+          </div>
+        </div>
+        <div>
+          <h1 className=" font-display font-bold px-3 mt-1 ">
+            Palestra sobre Virtual Reality
+          </h1>
+          <div className="flex text-[15px]">
+            <IoIosPin className="text-light-grey ml-2 mt-1" />
+            <p className=" font-normal  font-display text-light-grey ml-1">
+              Evento Presencial,
+            </p>
+            <p className=" font-normal font-display text-light-grey ml-1">
+              Começa em 29 de Mar, Sexta-Feira, 2024 22:00 - 2h
+            </p>
+          </div>
         </div>
       </div>
-      <div className="bg-dark-grey w-full h-full rounded-t-3xl px-6 py-2 flex flex-col">
-        <div className="rounded-t-lg bg-light-purple w-[60px] h-[30px] flex text-center items-center">
-          <p className="font-display text-white font-normal text-[15px] p-4">
-            Mar
-          </p>
-        </div>
-        <div className="bg-white rounded-b-lg  w-[60px] h-[30px] flex text-center items-center">
-          <p className="font-display text-black font-bold text-[20px] p-[1.20rem]">
-            29
-          </p>
+      <div className="flex">
+        <p className=" font-normal font-display text-light-grey ml-1 text-[18px] py-5">
+          Venha de encontro conosco nesta jornada sobre Realidade Virtual!
+          teremos participações dos maiores especialistas na área, não perca!
+        </p>
+
+        <div>
+          {/* TODO: @PvtWendy ajustar aquyi o google maps conforme o protótipo do figma, de preferência funcionando. */}
         </div>
       </div>
     </div>
