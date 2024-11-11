57cf1c904aad65dd775c71e389da4ceda5d88a45
Monique de Jesus Silva <rizzarddirizu@gmail.com>
2024-11-10 21:18:04 -0400

    refactor: ajuste na estilização e importação do componente EventDetailsCard.

--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -5,12 +5,13 @@ import { Avatar } from '@/components/ui/avatar'
 import { RiVerifiedBadgeFill } from 'react-icons/ri'
 import { IoIosPin } from 'react-icons/io'
 import TranslucentCard from '@/components/app/translucentcard'
+import EventDetailsCard from '@/components/app/event-details-card'
 
 export default function EventDetails() {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <TranslucentCard />
-      <div className=" bg-dark-grey w-full h-full rounded-t-3xl px-2 py-4  flex ">
+      <div className=" bg-dark-grey w-full h-full rounded-t-3xl px-5 py-4  flex ">
         <div className="flex flex-col">
           <div className="rounded-t-lg bg-light-purple w-[60px] h-[30px] flex text-center items-center">
             <p className="font-display text-white font-normal text-[15px] p-4">
@@ -23,22 +24,20 @@ export default function EventDetails() {
             </p>
           </div>
         </div>
-        <div>
+        <div className="px-2">
           <h1 className=" font-display font-bold px-3 mt-1 ">
             Palestra sobre Virtual Reality
           </h1>
-          <div className="flex text-[15px]">
+          <div className="flex text-[15px] ">
             <IoIosPin className="text-light-grey ml-2 mt-1" />
             <p className=" font-normal  font-display text-light-grey ml-1">
-              Evento Presencial,
-            </p>
-            <p className=" font-normal font-display text-light-grey ml-1">
-              Começa em 29 de Mar, Sexta-Feira, 2024 22:00 - 2h
+              Evento Presencial, Começa em 29 de Mar, Sexta-Feira, 2024 22:00 -
+              2h
             </p>
           </div>
         </div>
       </div>
-      <div className="flex">
+      <div className="flex px-5">
         <p className=" font-normal font-display text-light-grey ml-1 text-[18px] py-5">
           Venha de encontro conosco nesta jornada sobre Realidade Virtual!
           teremos participações dos maiores especialistas na área, não perca!
@@ -47,6 +46,10 @@ export default function EventDetails() {
         <div>
           {/* TODO: @PvtWendy ajustar aquyi o google maps conforme o protótipo do figma, de preferência funcionando. */}
         </div>
+      </div>
+
+      <div className="px-5">
+        <EventDetailsCard />
       </div>
     </div>
   )
